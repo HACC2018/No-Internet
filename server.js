dcf6f897363641dab20b487e89cfb677cd3f6fe7
Zak Sakata <zakattack26111@gmail.com>
2018-11-08 02:03:29 -0700

    added favicon and pushing up final changes for deplyoment

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const session = require('express-session');
 const Redis = require('connect-redis')(session);
 const passport = require('passport');
 const exphbs = require('express-handlebars');
+const favicon = require('serve-favicon');
+// const functions = require('firebase-functions');
 // const http = require('http').Server(server);
 const PORT = process.env.PORT || 8080;
 const server = express();
-const routes = require('./server/db/routes/');
+//const routes = require('./server/db/routes/');
 
 //middleware
 server.use(express.static(__dirname + '/public')); //load static files (css & js)
+server.use(favicon(__dirname + '/public/images/haawi.jpg'));
 // server.use(express.bodyParser());
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({extended: true}));
@@ -47,8 +50,8 @@ function checkAuth (req, res, next) { //prevents routes from being accessed with
 // });
 
 server.get('/', function (req, res) { //loads login page by default
-    res.render('home');
-  });
+  res.render('home');
+});
 
 server.get('/register', function (req, res) { //loads register page
   res.render('register');
@@ -62,9 +65,10 @@ server.get('/search', function (req, res) { //loads register page
   res.render('search');
 });
 
+// exports.home = functions.https.onRequest(server);
 
 
-server.use('/', routes);
+//server.use('/', routes);
 
 const websock = server.listen(PORT, () => {
   console.log(`Connected to port ${PORT}\n`);
